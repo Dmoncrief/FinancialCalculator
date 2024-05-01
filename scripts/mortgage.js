750510911553c8d0a5d2867a636464bc5c660938
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-01 10:30:40 -0400

    Fixed buttons

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -1,13 +1,12 @@
-"use strict";
-
 window.onload = init;
-
 
 // Input --------------------
 
 const principleTextbox = document.getElementById("principleTextbox");
 const interestRateTextbox = document.getElementById("interestRateTextbox");
-const loanLengthYearsTextbox = document.getElementById("loanLengthYearsTextboc");
+const loanLengthYearsTextbox = document.getElementById(
+  "loanLengthYearsTextbox"
+);
 
 // button -----------------------
 const calculateButton = document.getElementById("calculateButton");
@@ -15,58 +14,51 @@ const resetButton = document.getElementById("resetButton");
 
 // output-----------------------
 
-const outputTextbox = document.getElementById("outputTextbook");
+const outputTextbox = document.getElementById("outputTextbox");
 
 function init() {
-    
-    
-    calculateButton.onclick = onCalculatorButtonClicked;
-    resetButton.onclick = onResetButtonClicked;
+  calculateButton.onclick = onCalculatorButtonClicked;
+  resetButton.onclick = onResetButtonClicked;
 }
-
-
-
 
 function onCalculatorButtonClicked() {
-    // variables
-    let monthlyPayment;
-    let totalInterest;
-    let equation1;
-     let equation2;
+  // variables
+  let monthlyPayment;
+  let totalInterest;
+  let equation1;
+  let equation2;
+
+  // variables from values----------------------------------
+  let principleLoanAmount = Number(principleTextbox.value);
+  let interestRate = Number(interestRateTextbox.value) / 100;
+  let monthlyInterestRate = interestRate / 12;
+  let loanLengthMonths = Number(loanLengthYearsTextbox.value) * 12;
+
+  // input number---------------------------------
+  if (
+    Number.isNaN(principleLoanAmount) ||
+    Number.isNaN(interestRate) ||
+    Number.isNaN(loanLengthMonths)
+  ) {
+    // for error message for output
+
+    outputElement.value = "Please make sure you enter numbers in all fields";
+    outputElement.setAttribute("class", "form-control-plaintext text-danger");
+  } else {
+    equation1 =
+      monthlyInterestRate * (1 + monthlyInterestRate) ** loanLengthMonths;
+    equation2 = (1 + monthlyInterestRate) ** loanLengthMonths - 1;
+    monthlyPayment = principleLoanAmount * (equation1 / equation2);
+    totalInterest = monthlyPayment * loanLengthMonths - principleLoanAmount;
+    outputTextbox.setAttribute("class", "form-control-plaintext");
+    outputTextbox.value =
+      `A $${principleLoanAmount} loan at ${interestRate * 100}% interest for ${loanLengthMonths / 12} years would have a $ ${monthlyPayment.toFixed(2)}`;
+  }
 }
-
-// variables from values----------------------------------
-let principleLoanAmount = Number(principleTextbox.value);
-let interestRate = Number(interest.value) / 100;
-let monthlyInterestRate = interestrate / 12;
-let loanLengthMonths = Number(loanLength.value) * 12;
-
-
-
-// input number---------------------------------
-if (Number.isNaN(principleLoanAmount) || Number.isNaN(interestRate) || Number.isNaN(loanLength)) {
-
-
-// for error message for output
-
-outputElement.value = "Please make sure you enter numbers in all fields";
-outputElement.setAttribute("class", "form-control-plaintext text-danger");
-
-
-}
-else{
-    equation1 = monthlyInterestRate * (1 + monthlyInterestRate) ** loanLengthMonths;
-    equation2 = ((1 + monthlyInterestRate) ** loanLengthMonths) - 1;
-    monthlyPayment = principleloanAmount * (equation1 / equation2);
-    totalInterest = (monthlyPayment * loanLengthMonths) - principleLoanAmount;
-    outputElement.setAttribute("class", "form-control-plaintext");
-    outputElement.value = 'A $'${principleAmount} "loan at" ${interestRate * 100}% "interest for" ${loanLengthMonths / 12} 'years would have a $'${monthlyPayment.tofixed(2)};
-}
-
 // clears all fields----------------------------
-function onResetButtonClicked(){
-    principleTextbox.value = "";
-    interestRateTextbox.value = "";
-    loanLengthYearsTextbox.value = "";
-    outputTextbox.value = "";
+function onResetButtonClicked() {
+  principleTextbox.value = "";
+  interestRateTextbox.value = "";
+  loanLengthYearsTextbox.value = "";
+  outputTextbox.value = "";
 }
